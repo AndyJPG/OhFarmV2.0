7cacb5d9dd48611e7217f81f16a8344fd6528fd8
Peigeng Jiang Andy <jiangpeigeng@gmail.com>
2019-04-15 15:53:55 +1000

    Initial Commit

---
+++ OhFarmV2.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OhFarmV2.0
+//
+//  Created by Peigeng Jiang on 15/4/19.
+//  Copyright © 2019 Peigeng Jiang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OhFarmV2.0Tests/OhFarmV2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  OhFarmV2_0Tests.swift
+//  OhFarmV2.0Tests
+//
+//  Created by Peigeng Jiang on 15/4/19.
+//  Copyright © 2019 Peigeng Jiang. All rights reserved.
+//
+
+import XCTest
+@testable import OhFarmV2_0
+
+class OhFarmV2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
